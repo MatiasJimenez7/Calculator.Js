2bcc55cfabd6edbefaa36455dec2115feb914e94
MatiasJimenez7 <jimenezmatias007@gmail.com>
2022-08-23 17:31:48 -0300

    Calculator

---
+++ JavaScript.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", ()=> {
+    const display = document.querySelector(".display-calculadora")
+    const keypadButtons = document.getElementsByClassName("keypad-button")
+    const botonesArray = Array.from(keypadButtons)
+    botonesArray.forEach((boton)=> {
+        boton.addEventListener("click", ()=> {
+            calculadora(boton, display);
+        })
+    })
+})
+function calculadora (boton, display) {
+    switch (boton.innerHTML) {
+        case "C":
+            borrar(display)
+            break;
+        case "=": 
+            calcular(display);
+            break;
+        default:
+            actualizar(display, boton)
+            break    
+            
+    }
+}
+
+function borrar(display) {
+    display.innerHTML = 0
+}
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, boton) {
+    if(display.innerHTML == 0) {
+        display.innerHTML = "";
+    }
+    display.innerHTML += boton.innerHTML;
+}
